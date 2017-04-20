d0db501d8232afaefa2deac6073942cf38d5addb
Darius Musiolik <MrFlyingToasterman@yahoo.de>
2017-04-20 18:54:15 +0200

    Fixed SQL connection

---
+++ inc_mysql.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "localhost";
+    $user = "Rainer";
+    $pass = "data";
+    $database = "HowdyDB";
+    $dz = mysql_connect($host, $user, $pass);
+    mysql_select_db($database, $dz);
+?>
--- login_window.php
+++ login_window.php
@@ -1,12 +1,41 @@
 <div class="login_window_container">
   <section class="well">
   <img class="animated wobble logo" src="images/logo_text_transparent.png">
-  <form name="login_form" method="post" enctype="multipart/form-data" action="chat_window.php">
+  <form name="login_form" method="post" enctype="multipart/form-data" action="index.php">
     <p>Username</p>
-    <p><input type="text" name="user_name"></p>
+    <p><input type="text" name="user_name" required="true"></p>
     <p>Password</p>
-    <p><input type="password" name="user_password"></p>
+    <p><input type="password" name="user_password" required="true"></p>
     <p><input class="btn btn-primary" name="login_submit" type="submit" value="login"></p>
   </form>
+  <?php
+    if (isset($_POST["user_name"])) {
+
+      //SQL Lesen vorbereiten
+      $userwho = $_POST["user_name"];
+      $userpasswordwho = $_POST["user_password"];
+      $drop_error = true;
+
+      //SQL Lesen
+            include "inc_mysql.php"; //Verbindung zum Server aufbauen
+
+            $query = "SElECT * FROM tbl_Benutzer"; //SQL Anfrage
+            $sql = mysql_query($query); //Daten Anfordern
+            while($ds = mysql_fetch_array($sql)) {
+
+                //Prüfe ob Benutzername aus der textbox gefunden worden ist.
+                if ($ds["Username"] == $userwho) {
+                    if ($userpasswordwho  == $ds["Passwort"]) {
+                      //Login Erfolgreich!
+                      $drop_error = false;
+                      // sesson erstellen mit nutzernamen; chat_window aufrufen;
+                    }
+                }
+              }
+              if ($drop_error) echo "<p style=\"color:red;\"><b> ACCESS DENIED! </b></p>";
+
+            mysql_close($dz); //Verbindung zum Server beenden
+    }
+  ?>
   </section>
 </div>
