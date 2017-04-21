a8d22894e97ce477953a2611e7c5580a77a4929b
Pascal3366 <passi0698@gmx.de>
2017-04-21 10:05:33 +0200

    did something with chatwindow

--- chat_window.php
+++ chat_window.php
@@ -6,10 +6,93 @@
 
 <!DOCTYPE html>
 <html>
-<head></head>
+<head>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/chat_window.min.css">
+</head>
 <body>
 
-  
+  <div class="container-fluid">
+<div class="row">
+<div class="col-md-12">
+  <div class="page-header">
+    <img class="logo" src="images/logo_text_transparent.png">
+    <p class="pull-right">
+      Have you already Howdy'd today?
+    </p>
+  </div>
+  <nav class="navbar navbar-default" role="navigation">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#"></a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#"></a></li>
+
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">Logged in as ... Logout?</a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span></a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+</div>
+</div>
+<div class="row">
+<div class="col-md-4">
+  <div class="list-group">
+     <a href="#" class="list-group-item active">Home</a>
+    <div class="list-group-item">
+      List header
+    </div>
+    <div class="list-group-item">
+      <h4 class="list-group-item-heading">
+        List group item heading
+      </h4>
+      <p class="list-group-item-text">
+        ...
+      </p>
+    </div>
+    <div class="list-group-item">
+      <span class="badge">14</span>Help
+    </div> <a class="list-group-item active"><span class="badge">14</span>Help</a>
+  </div>
+</div>
+<div class="col-md-8">
+  <h2>
+    Heading
+  </h2>
+  <p>
+    Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+  </p>
+  <p>
+    <a class="btn" href="#">View details »</a>
+  </p>
+</div>
+</div>
+<div class="row footer">
+<div class="col-md-12">
+  <div class="navbar-fixed-bottom">
+    <div class="container-fluid">
+      <p class="text-center text-muted">
+        &copy; 2017 - 2017 Pascal Peinecke &amp; Darius Musiolik.
+      </p>
+    </div>
+  </div>
+</div>
+</div>
+</div>
 
 </body>
 </html>
