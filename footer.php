7a4d4b07bf338f45bec9799d17fd1c6c2a8c2520
Pascal3366 <passi0698@gmx.de>
2017-05-07 23:10:54 +0200

    reinvented the wheel

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="navbar navbar-fixed-bottom">
-  <div class="container">
-    <p> Copyright &copy;<br>Darius Musiolik &amp; Pascal Peinecke <br>2017 - <script>document.write(new Date().getFullYear())</script> </p>
-  </div>
-</footer>
