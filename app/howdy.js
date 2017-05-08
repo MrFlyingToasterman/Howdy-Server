a3f47d6b615d04028a69d7e85648e1188badf89d
Pascal3366 <passi0698@gmx.de>
2017-05-09 00:04:13 +0200

    added ssl + redirection from http, added crypto and certs

--- app/howdy.js
+++ app/howdy.js
@@ -4,11 +4,23 @@ const express       = require('express');
 const passport      = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const flash         = require('connect-flash');
-const app           = express();
-const node_port          = 1701;
+const fs            = require('fs');
+const https         = require('https');
+const http          = require('http');
+const node_port     = 1701;
 
 // Include Authentication Strategies
 require('./config/passport/passport');
+
+var app = express();
+
+
+// Enable reverse proxy support in Express. This causes the
+// the "X-Forwarded-Proto" header field to be trusted so its
+// value can be used to determine the protocol. See
+// http://expressjs.com/api#app-settings for more details.
+app.enable('trust proxy');
+
 
 app.set('view engine', 'jade');
 app.set('views', './app/views');
@@ -21,13 +33,30 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
+app.use(function(req,res,next) {
+    if(req.headers["x-forwarded-proto"] == "http") {
+        res.redirect("https://localhost" + req.url);
+    } else {
+        return next();
+    }
+});
+
 // Include all Routes
 require('./config/routes/routes')(app);
 
 // Change port if you'd like here
-const server = app.listen(node_port, function () {
-  const port = server.address().port;
-  console.log('Listening at http://localhost:%s', port);
-});
+//const server = app.listen(node_port, function () {
+//  const port = server.address().port;
+//  console.log('Listening at http://localhost:%s', port);
+//});
 
-module.exports.getApp = app;
+var sslOptions = {
+  key: fs.readFileSync('./app/ssl/key.pem'),
+  cert: fs.readFileSync('./app/ssl/cert.pem'),
+  passphrase: 'datadata'
+};
+
+var net=require('net');
+var handle=net.createServer().listen(8000);
+
+https.createServer(sslOptions, app).listen(handle);
